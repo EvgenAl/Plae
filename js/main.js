d1ea18b3e63beda87c3d7f4eac4a39555e625ba6
Spinto <evgenii_krishtopa@mail.ru>
2016-11-16 19:16:22 +0200

    init

---
+++ js/main.js
@@ -0,0 +1,20 @@
+/* Libs
+===========================*/
+
+//= ../../bower_components/jquery/dist/jquery.min.js
+
+
+
+/* bootstrap
+===========================*/
+
+//= bootstrap/bootstrap.js
+
+
+
+
+/* Custom
+===========================*/
+
+//= custom/custom.js
+
